f903c8f9db39aa13661ef3100ffa8262f864232d
Dave Hall <dave@etianen.com>
2017-08-07 10:42:56 +0100

    Setting explicit pyasn version

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         "django>=1.8",
-        "ldap3==2.2.3",
+        "ldap3==2.3",
+        "pyasn1==0.3.2",
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
